01e13e3849a75cefd788c44c3b1a999a7e48a760
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-01-07 20:29:07 -0500

    tried adding more code

--- main_js_files/Employee.js
+++ main_js_files/Employee.js
@@ -1,40 +1,45 @@
-// create an employee class
+
+// It is recommended that you follow this workflow:
+
+// 1. Run tests
+// 2. Create or update classes to pass a single test case
+// 3. Repeat
+
+// 🎗 Remember, you can run the tests at any time with `npm run test`
 
 // export the employee
 
-
-//constructor defines the properties of the object "employee". They can change!
 class Employee {
     constructor(name, id, email) {
         this.name = name;
         this.id = id;
         this.email = email;
     }
+    // handy-dandy constructor which defines the properties of the object "employee"
 
     getName(){
-        console.log
+        console.log(`This employee's name is ${this.name}.`);
     }
 
     getId(){
-
+        console.log(`Their ID is: ${this.id}`);
     }
 
     getEmail(){
-
+        console.log(`Their email address is: ${this.email}`);
     }
 
     getRole(){
-        
+        console.log(`${Employee}`);
     }
 }
 
+const alice = new Employee("Alice", 1, "alice@gmail.com");
+
+alice.getName();
+alice.getId();
+alice.getEmail();
+alice.getRole();
+
+
 module.exports = Employee;
-
-
-// The first class is an `Employee` parent class with the following properties and
-// methods:
-
-//   * getName()
-//   * getId()
-//   * getEmail()
-//   * getRole() // Returns 'Employee'
