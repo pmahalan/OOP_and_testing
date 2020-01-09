ccbfdb169a009466f2baae2c1c28e954e8429a42
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-01-08 21:32:16 -0500

    created an 'output' folder, finally got the HTML page to render upon the running of the program and the user's finishing creating people

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 const Engineer = require("./main_js_files/Engineer");
 const Intern = require("./main_js_files/Intern");
 const Manager = require("./main_js_files/Manager");
+
 const path = require("path");
 const inquirer = require("inquirer")
 const fs = require("fs");
+
 const engineerCard = require("./render_cards/engineerCard");
 const internCard = require("./render_cards/internCard");
 const managerCard = require("./render_cards/managerCard");
+
 const mainRender = require("./render_cards/mainRender");
 
 
@@ -43,22 +46,44 @@ function mainApp() {
         ])
         .then(answers =>{
             const {managerName, managerId, managerEmail, managerNumber} = answers;
-            const managerObj = newManager(managerName, managerId, managerEmail, managerNumber);
-            const managerCard = managerCard(managerObj);
+            const managerObj = new Manager(managerName, managerId, managerEmail, managerNumber);
+            const managerCardhtml = managerCard(managerObj);
             teamMember.push(managerCardhtml);
+
+            createTeam();
             
         })
 
-}
-// this function create a list to add teammembers
+        }
+
+        // this function create a list to add teammembers
 function createTeam(){
 
     inquirer
         .prompt([
-            
+            {
+                type: "list",
+                message: "What type of team member would you like to add?",
+                choices: [
+                    "engineer",
+                    "intern",
+                    "build team"
+                ],
+                name: "teamMembers",
+            }
         ])
         .then(answers => {
             // create a switch statement to choose between engineer, intern, or build team
+            switch(answers.teamMembers)
+
+            {
+                case "engineer": getEngineer()
+                break;
+                case "intern": getIntern()
+                break;
+                case "build team": buildTeam();
+            }
+                
            
         })
 }
@@ -68,23 +93,74 @@ function getEngineer() {
 
     inquirer    
         .prompt([
-            
+            {
+                type: "input",
+                message: "What is the engineers's name?",
+                name: "enName"
+            },
+            {
+                type: "input",
+                message: "What is the engineer's ID?",
+                name: "enId"
+            },
+            {
+                type: "input",
+                message: "What is the engineer's email address?",
+                name: "enEmail"
+            },
+            {
+                type: "input",
+                message: "What is the engineer's GitHub username?",
+                name: "enGh"
+            }
 
         ])
         .then(answers =>{
+            const {enName, enId, enEmail, enGh} = answers;
+            const EngineerObj = new Engineer(enName, enId, enEmail, enGh);
+            const engineerCardhtml = engineerCard(EngineerObj);
+            teamMember.push(engineerCardhtml)
+
+            createTeam();
            
         })
+    }
 
-}
 // a function that create an intern
 function getIntern() {
 
     inquirer    
         .prompt([
+            {
+                type: "input",
+                message: "What is the intern's name?",
+                name: "iName"
+            },
+            {
+                type: "input",
+                message: "What is the intern's ID?",
+                name: "iId"
+            },
+            {
+                type: "input",
+                message: "What is the intern's email address?",
+                name: "iEmail"
+            },
+            {
+                type: "input",
+                message: "What is the intern's school?",
+                name: "iSchool"
+            }
           
 
         ])
         .then(answers =>{
+            const {iName, iId, iEmail, iSchool} = answers;
+            const InternObj = new Intern(iName, iId, iEmail, iSchool);
+            const internCardhtml = internCard(InternObj);
+            teamMember.push(internCardhtml)
+
+            createTeam();
           
         })
 
--- render_cards/internCard.js
+++ render_cards/internCard.js
@@ -4,14 +4,14 @@ function internCard(data){
     return `
     <div class="card employee-card">
     <div class="card-header">
-        <h2 class="card-title">{{ name }}</h2>
-        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>{{ role }}</h3>
+        <h2 class="card-title">${name}</h2>
+        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${getRole()}</h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
-            <li class="list-group-item">ID: {{ id }}</li>
-            <li class="list-group-item">Email: <a href="mailto:{{ email }}">{{ email }}</a></li>
-            <li class="list-group-item">School: {{ school }}</li>
+            <li class="list-group-item">ID: ${id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+            <li class="list-group-item">School: ${ school }</li>
         </ul>
     </div>
 </div>
@@ -19,4 +19,6 @@ function internCard(data){
 `
 
 }
+
+module.exports = internCard
 // export internCard
