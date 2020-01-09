381deb6f460c537f30676f662813322906e462e1
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2020-01-08 21:46:37 -0500

    ran the CL and did one of each type of employee to check for little errors; made quick, last-minute fixes.

--- render_cards/engineerCard.js
+++ render_cards/engineerCard.js
@@ -5,7 +5,7 @@ function engineerCard(data){
     <div class="card employee-card">
     <div class="card-header">
         <h2 class="card-title">${data.name}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>{${data.getRole()}</h3>
+        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${data.getRole()}</h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
